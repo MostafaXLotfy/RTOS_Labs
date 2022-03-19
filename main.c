74c0be5bca9beae1fe7494efe34b885784083d9b
Mostafa Lotfy <oed5med@gmail.com>
2022-03-19 12:04:47 +0200

    lab2

--- main.c
+++ main.c
@@ -8,7 +8,12 @@
 #include "driverlib/interrupt.h"
 #include "BSP.h"
 
+uint32_t toggle_blue_stack[40];
+uint32_t *sp_toggle_blue = &toggle_blue_stack[40];
 void toggle_blue(void);
+
+uint32_t toggle_red_stack[40];
+uint32_t *sp_toggle_red = &toggle_red_stack[40];
 void toggle_red(void);
 
 
@@ -34,9 +39,7 @@ void toggle_blue(void){
 
 
 int main()
-{
-	IntMasterDisable();
-	
+{	
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
   // initialize systick counter
@@ -46,8 +49,26 @@ int main()
   SysTickIntRegister(systick_handler);
 	
 	IntMasterEnable();
+	*(--sp_toggle_blue) = (1U << 24);  /* xPSR */
+  *(--sp_toggle_blue) = (uint32_t)&toggle_blue; /* PC */
+  *(--sp_toggle_blue) = 0x0000000EU; /* LR  */
+  *(--sp_toggle_blue) = 0x0000000CU; /* R12 */
+  *(--sp_toggle_blue) = 0x00000003U; /* R3  */
+  *(--sp_toggle_blue) = 0x00000002U; /* R2  */
+  *(--sp_toggle_blue) = 0x00000001U; /* R1  */
+  *(--sp_toggle_blue) = 0x00000000U; /* R0  */
+  /* fabricate Cortex-M ISR stack frame for blinky2 */
+  *(--sp_toggle_red) = (1U << 24);  /* xPSR */
+  *(--sp_toggle_red) = (uint32_t)&toggle_red; /* PC */
+  *(--sp_toggle_red) = 0x0000000EU; /* LR  */
+  *(--sp_toggle_red) = 0x0000000CU; /* R12 */
+  *(--sp_toggle_red) = 0x00000033U; /* R3  */
+  *(--sp_toggle_red) = 0x00000022U; /* R2  */
+  *(--sp_toggle_red) = 0x00000011U; /* R1  */
+  *(--sp_toggle_red) = 0x00000000U; /* R0  */
+  
+	toggle_red();
 	toggle_blue();
-  toggle_red();
   
   while(true){
     
