d9a80a5846df86551dc4770326d2a0859b280c58
Mostafa Lotfy <oed5med@gmail.com>
2022-03-26 22:32:01 +0200

    Lab4

--- main.c
+++ main.c
@@ -19,6 +19,8 @@
 #include "driverlib/interrupt.h"
 
 void toggle_red(void *pvParameters);
+void toggle_blue(void *pvParameters);
+void toggle_green(void *pvParameters);
 
 
 void toggle_red(void *pvParameters){
@@ -32,13 +34,36 @@ void toggle_red(void *pvParameters){
     vTaskDelete(NULL);
 }
 
+void toggle_blue(void *pvParameters){
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_2);
+    for(;;){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        vTaskDelay(2000 / portTICK_RATE_MS);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,0);
+        vTaskDelay(2000 / portTICK_RATE_MS);        
+    }
+    vTaskDelete(NULL);
+}
+
+void toggle_green(void *pvParameters){
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_3);
+    for(;;){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
+        vTaskDelay(3000 / portTICK_RATE_MS);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0);
+        vTaskDelay(3000 / portTICK_RATE_MS);        
+    }
+    vTaskDelete(NULL);
+}
 
 int main()
 {    
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
 		
-  xTaskCreate(toggle_red, "toggle_red", 240, NULL, 1, NULL);
+  xTaskCreate(toggle_red, "toggle_red", 120, NULL, 1, NULL);
+  xTaskCreate(toggle_blue, "toggle_blue", 120, NULL, 2, NULL);
+  xTaskCreate(toggle_green, "toggle_gree", 120, NULL, 3, NULL);
 
   vTaskStartScheduler();
   while(true){
