cc05c47e43ec60d59a823dbe826b7957715aafa0
Mostafa Lotfy <oed5med@gmail.com>
2022-04-19 15:46:22 +0200

    Lab5 version2

--- main.c
+++ main.c
@@ -20,8 +20,8 @@
 #include "tm4c123gh6pm.h"
 
 void toggle_red(void *pvParameters);
-void toggle_blue(void *pvParameters);
 void toggle_green(void *pvParameters);
+void toggle_blue(void *pvParameters);
 
 void toggle_red(void *pvParameters){
     for(;;){
@@ -40,6 +40,13 @@ void toggle_green(void *pvParameters){
     vTaskDelete(NULL);
 }
 
+void toggle_blue(void *pvParameters){
+    for(;;){
+				GPIO_PORTF_DATA_R ^= 04;
+        vTaskDelay(2000 / portTICK_RATE_MS);   
+    }
+    vTaskDelete(NULL);
+}
 void vApplicationIdleHook(){
 		GPIO_PORTF_DATA_R ^= 0x4;
 }
@@ -48,13 +55,14 @@ int main()
 {    
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
+		
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,GPIO_PIN_3 | GPIO_PIN_1 | GPIO_PIN_2);
-	GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 
 
   xTaskCreate(toggle_red, "toggle_red", 120, NULL, 1, NULL);
   xTaskCreate(toggle_green, "toggle_green", 120, NULL, 1
 		, NULL);
+
 
   vTaskStartScheduler();
   while(true){
